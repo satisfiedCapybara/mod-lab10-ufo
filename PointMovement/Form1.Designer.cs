f3c83ba0a03d2663dc911fa065ac590f494b9c75
semsmirn <semen.smirnov@opencascade.com>
2022-05-10 10:55:52 +0300

    [mod-lab10-ufo]
    Сreateв Windows Forms applications using graphics elements, simulate the movement of objects across the screen between two points.

---
+++ PointMovement/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace PointMovement
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+      this.y2 = new System.Windows.Forms.TextBox();
+      this.x2 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.lineCalculationRB = new System.Windows.Forms.RadioButton();
+      this.AngleRB = new System.Windows.Forms.RadioButton();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.Precision = new System.Windows.Forms.TextBox();
+      this.Value = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // y2
+      // 
+      this.y2.Location = new System.Drawing.Point(99, 50);
+      this.y2.Name = "y2";
+      this.y2.Size = new System.Drawing.Size(100, 22);
+      this.y2.TabIndex = 1;
+      this.y2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.y2_KeyPress);
+      // 
+      // x2
+      // 
+      this.x2.Location = new System.Drawing.Point(99, 14);
+      this.x2.Name = "x2";
+      this.x2.Size = new System.Drawing.Size(100, 22);
+      this.x2.TabIndex = 2;
+      this.x2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.x2_KeyPress);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.BackColor = System.Drawing.Color.Transparent;
+      this.label3.Location = new System.Drawing.Point(27, 15);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(37, 17);
+      this.label3.TabIndex = 6;
+      this.label3.Text = "X2 =";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.BackColor = System.Drawing.Color.Transparent;
+      this.label4.Location = new System.Drawing.Point(27, 53);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(41, 17);
+      this.label4.TabIndex = 7;
+      this.label4.Text = "Y2 = ";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(205, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(114, 55);
+      this.button1.TabIndex = 8;
+      this.button1.Text = "Start!";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // lineCalculationRB
+      // 
+      this.lineCalculationRB.AutoSize = true;
+      this.lineCalculationRB.BackColor = System.Drawing.Color.Transparent;
+      this.lineCalculationRB.Checked = true;
+      this.lineCalculationRB.Location = new System.Drawing.Point(325, 15);
+      this.lineCalculationRB.Name = "lineCalculationRB";
+      this.lineCalculationRB.Size = new System.Drawing.Size(115, 21);
+      this.lineCalculationRB.TabIndex = 9;
+      this.lineCalculationRB.TabStop = true;
+      this.lineCalculationRB.Text = "Line equation";
+      this.lineCalculationRB.UseVisualStyleBackColor = false;
+      // 
+      // AngleRB
+      // 
+      this.AngleRB.AutoSize = true;
+      this.AngleRB.BackColor = System.Drawing.Color.Transparent;
+      this.AngleRB.Location = new System.Drawing.Point(325, 49);
+      this.AngleRB.Name = "AngleRB";
+      this.AngleRB.Size = new System.Drawing.Size(97, 21);
+      this.AngleRB.TabIndex = 10;
+      this.AngleRB.Text = "With Angle";
+      this.AngleRB.UseVisualStyleBackColor = false;
+      this.AngleRB.CheckedChanged += new System.EventHandler(this.AngleRB_CheckedChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.Color.Transparent;
+      this.label1.Location = new System.Drawing.Point(27, 95);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(30, 17);
+      this.label1.TabIndex = 11;
+      this.label1.Text = "N =";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.BackColor = System.Drawing.Color.Transparent;
+      this.label2.Location = new System.Drawing.Point(27, 131);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(66, 17);
+      this.label2.TabIndex = 12;
+      this.label2.Text = "Precision";
+      // 
+      // Precision
+      // 
+      this.Precision.Location = new System.Drawing.Point(99, 92);
+      this.Precision.Name = "Precision";
+      this.Precision.Size = new System.Drawing.Size(100, 22);
+      this.Precision.TabIndex = 13;
+      this.Precision.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Precision_KeyPress);
+      // 
+      // Value
+      // 
+      this.Value.Location = new System.Drawing.Point(99, 128);
+      this.Value.Name = "Value";
+      this.Value.Size = new System.Drawing.Size(100, 22);
+      this.Value.TabIndex = 14;
+      this.Value.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Value_KeyPress);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.Value);
+      this.Controls.Add(this.Precision);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.AngleRB);
+      this.Controls.Add(this.lineCalculationRB);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.x2);
+      this.Controls.Add(this.y2);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Name = "MainForm";
+      this.Text = "Point Movement";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.TextBox y2;
+    private System.Windows.Forms.TextBox x2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.RadioButton lineCalculationRB;
+    private System.Windows.Forms.RadioButton AngleRB;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox Precision;
+    private System.Windows.Forms.TextBox Value;
+  }
+}
+
---
+++ PointMovement/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointMovement
+{
+  public partial class MainForm : Form
+  {
+    private Thread thread;
+    public MainForm()
+    {
+      InitializeComponent();
+      Value.Enabled = false;
+      Precision.Enabled = false;
+    }
+
+    public void WithAngle(object obj)
+    {
+      int[] arr = obj as int[];
+      int x2 = arr[0], y2 = arr[1], value = arr[2], precision = arr[3];
+      int x1 = 0, y1 = 0;
+      double x = 0, y = 0;
+      int step = 5;
+
+      Pen pen = new Pen(Color.Black, 2);
+      Pen pen2 = new Pen(Color.Red, 5);
+      Pen pen3 = new Pen(Color.Blue, 5);
+
+
+      double angle = Math.Atan(Math.Abs(y2 - y1) / (double)Math.Abs(x2 - x1)) * 180 / Math.PI;
+      double distance = Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2));
+      double sin = Sin(angle, precision);
+      double cos = Cos(angle, precision);
+
+      Graphics graphics = CreateGraphics();
+      graphics.TranslateTransform(Width / 2 + 20, Height / 2 - 10);
+      graphics.ScaleTransform(0.5f, 0.5f);
+      graphics.DrawEllipse(pen3, x2 - value / 2, y2 - value / 2, value * 2, value * 2);
+
+      while (distance > value)
+      {
+        Thread.Sleep(50);
+        x += step * cos;
+        y += step * sin;
+        graphics.DrawLine(pen, new Point(x1, y1), new Point(x2, y2));
+        graphics.DrawEllipse(pen2, new Rectangle((int)x, (int)y, 1, 1));
+        double previousDistance = distance;
+        distance = Math.Sqrt(Math.Pow(y2 - y, 2) + Math.Pow(x2 - x, 2));
+
+        if (previousDistance < distance)
+        {
+          MessageBox.Show("Промах!");
+          return;
+        }
+      }
+      MessageBox.Show("Попадание!");
+    }
+
+    public void LineEquation(object obj)
+    {
+      int[] arr = obj as int[];
+      int x1 = 0, y1 = 0;
+      int x2 = arr[0], y2 = arr[1];
+      double k = ((double)(y2 - y1)) / (x2 - x1);
+      double b = y1 - k * x1;
+
+      Graphics graphics = CreateGraphics();
+      Pen pen = new Pen(Color.Black, 2);
+      Pen pen2 = new Pen(Color.Red, 5);
+      graphics.TranslateTransform(Width / 2 + 20, Height / 2 - 10);
+
+      for (int x = x1; x <= x2; ++x)
+      {
+        Thread.Sleep(30);
+        graphics.DrawLine(pen, new Point(x1, y1), new Point(x2, y2));
+        graphics.DrawEllipse(pen2, new RectangleF(x, (float)(k*x + b), 1, 1));
+        graphics.FillEllipse(new SolidBrush(Color.Red), new RectangleF(x, (float)(k * x + b), 1, 1));
+      }
+    }
+
+    private double Sin(double angle, int precision)
+    {
+      double sin = 0;
+      for (int i = 0; i < precision; ++i)
+      {
+        int factorial = 1;
+        for (int j = 0; j < 2 * (i + 1); ++j)
+        {
+          if (i == 0) break;
+          factorial *= (j + 1);
+        }
+
+        if (i % 2 == 0)
+        {
+          sin += Math.Pow(angle / 180 * Math.PI, 2 * i + 1) / factorial;
+        }
+        else
+        {
+          sin -= Math.Pow(angle / 180 * Math.PI, 2 * i + 1) / factorial;
+        }
+      }
+      return sin;
+    }
+
+    private double Cos(double angle, int precision)
+    {
+      double cos = 0;
+      for (int i = 0; i < precision; ++i)
+      {
+        int factorial = 1;
+        for (int j = 0; j < 2 * i; ++j)
+        {
+          factorial *= (j + 1);
+        }
+
+        if (i % 2 == 0)
+        {
+          cos += Math.Pow(angle / 180 * Math.PI, 2 * i) / factorial;
+        }
+        else
+        {
+          cos -= Math.Pow(angle / 180 * Math.PI, 2 * i) / factorial;
+        }
+      }
+      return cos;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (x2.Text== "" || y2.Text == "" || Value.Text == "" || Precision.Text == "")
+      {
+        MessageBox.Show("Нерпавильно введены данные");
+        return;
+      }
+
+      if (!AngleRB.Checked)
+      {
+        thread = new Thread(LineEquation);
+        thread.Start(new int[] { Int32.Parse(x2.Text), Int32.Parse(y2.Text) });
+      }
+      else
+      {
+        thread = new Thread(WithAngle);
+        thread.Start(new int[] { Int32.Parse(x2.Text), Int32.Parse(y2.Text),
+                                 Int32.Parse(Value.Text), Int32.Parse(Precision.Text) });
+      }
+    }
+
+
+    private void x2_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      char number = e.KeyChar;
+
+      if (!Char.IsDigit(number))
+      {
+        e.Handled = true;
+      }
+    }
+
+    private void y2_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      char number = e.KeyChar;
+
+      if (!Char.IsDigit(number))
+      {
+        e.Handled = true;
+      }
+    }
+
+    private void AngleRB_CheckedChanged(object sender, EventArgs e)
+    {
+      if (AngleRB.Checked)
+      {
+        Value.Enabled = true;
+        Precision.Enabled = true;
+      }
+      else
+      {
+        Value.Enabled = false;
+        Precision.Enabled = false;
+      }
+    }
+
+    private void Precision_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      char number = e.KeyChar;
+
+      if (!Char.IsDigit(number))
+      {
+        e.Handled = true;
+      }
+    }
+
+    private void Value_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      char number = e.KeyChar;
+
+      if (!Char.IsDigit(number))
+      {
+        e.Handled = true;
+      }
+    }
+
+    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (thread != null)
+      {
+        thread.Join();
+      }
+    }
+  }
+}
